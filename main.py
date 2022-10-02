f036ef793e1c62e8164e95da4c61b65f32c804c1
Allie Shafer <allie@mnshafers.com>
2022-10-02 07:59:35 -0500

    Change character limit for register form

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ Base = declarative_base()
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
 
     # This will act like a List of BlogPost objects attached to each User.
     # The "author" refers to the author property in the BlogPost class.
